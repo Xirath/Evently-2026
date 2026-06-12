d17ed7ed09c46996c316b61419310e90e56137c9
Daniel Östberg <danielostberg@hotmail.com>
2026-06-12 10:13:59 +0200

    Worked on navbar accessibility and styling

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btnClose = document.querySelector("#btnClose");
 const media = window.matchMedia("(width < 1000px)");
 const topNavMenu = document.querySelector(".navbar__menu");
 const main = document.querySelector("main");
+const footer = document.querySelector("footer");
 
 function setupTopNav(e) {
   if (e.matches) {
@@ -13,20 +14,30 @@ function setupTopNav(e) {
     console.log("is desktop");
     topNavMenu.removeAttribute("inert");
     main.removeAttribute("inert");
+    footer.removeAttribute("inert");
+    btnOpen.setAttribute("aria-expanded", "false");
+    btnOpen.setAttribute("tabindex", "0");
   }
 }
 
 function openMobileMenu() {
   btnOpen.setAttribute("aria-expanded", "true");
+  btnOpen.setAttribute("tabindex", "-1");
   topNavMenu.removeAttribute("inert");
   topNavMenu.removeAttribute("style");
   main.setAttribute("inert", "");
+  footer.setAttribute("inert", "");
+  setTimeout(() => {
+    document.getElementById("home").focus();
+  }, 500);
 }
 
 function closeMobileMenu() {
   btnOpen.setAttribute("aria-expanded", "false");
+  btnOpen.setAttribute("tabindex", "0");
   topNavMenu.setAttribute("inert", "");
   main.removeAttribute("inert");
+  footer.removeAttribute("inert");
 
   setTimeout(() => {
     topNavMenu.style.transition = "none";
